b25c94009a1437e11def314adcbe03091a1034d9
pauma35 <startupway2013@gmail.com>
2026-04-16 16:12:47 +0900

    first commit

---
+++ avg.py
@@ -0,0 +1,24 @@
+import sys
+
+def avg_3(lst):
+    total = 0
+    count = 0
+
+    for x in lst:
+        if x % 3 == 0:
+            total += x
+            count += 1
+
+    if count == 0:
+        return 0
+
+    return total / count
+
+# コマンドライン引数を取得
+args = sys.argv[1:]
+
+# 文字列 → 数値に変換
+nums = [int(x) for x in args]
+
+# 実行
+print(avg_3(nums))
